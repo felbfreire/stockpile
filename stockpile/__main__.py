386057e502f169561ef1326c1b67a476365b444b
felbfreire <felbfreire@proton.me>
2023-07-28 17:09:12 -0300

    update __main__

--- stockpile/__main__.py
+++ stockpile/__main__.py
@@ -1,12 +1,7 @@
-from config import JSON_FILE_PATH
-from data import read_data
+from main import main
 
 
 if __name__ == '__main__':
     print('\nHello from Mr. Thomas Stockpile\n')
-
-    data_dic = read_data(JSON_FILE_PATH)
-
-    for key in data_dic:
-        print('%s: %s' % (key, data_dic[key]))
+    main()
 
