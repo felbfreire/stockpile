1462dca3efc2d7984871d4883bccd3bcb9820b64
felbfreire <felbfreire@proton.me>
2023-07-29 15:13:27 -0300

    add write_data to cli

--- stockpile/main.py
+++ stockpile/main.py
@@ -11,3 +11,12 @@ def main():
         for key in data_dic:
             print('%s: %s' % (key, data_dic[key]))
 
+    if argv[1] == 'write_data':
+        new_data = dict()
+        json_data = read_data(JSON_FILE_PATH)
+
+        key, value = argv[2].split('=')
+        new_data[key] = value
+        json_data.update(new_data)
+
+        write_data(json_data, JSON_FILE_PATH)
