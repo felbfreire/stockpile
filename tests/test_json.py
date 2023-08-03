d7ebfcd2125c0e86401c30855bda75bfa15de8b7
Felipe Freire <felbfreire@proton.me>
2023-08-03 08:32:57 -0300

    update write_data test

--- tests/test_json.py
+++ tests/test_json.py
@@ -9,14 +9,14 @@ TEST_FILE = 'json_test.json'
 class TestData:
 
     def test_read_data_to_json_file_writes_on_file(self):
-        data = {'foo': 'bar'}
+        data = {'foo': 'bar', 'goo': 'car'}
 
         write_data(data, TEST_FILE)
 
         expected_data = read_data(TEST_FILE)
         os.system('rm %s' % (TEST_FILE))
 
-        assert expected_data == {"foo": "bar"}
+        assert expected_data == {"foo": "bar", "goo": "car"}
 
 
     def test_update_data_updates_json_file(self):
