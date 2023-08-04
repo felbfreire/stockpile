ad3c58c2bc4ee54212f33426626a00e32b298939
Felipe Freire <felbfreire@proton.me>
2023-08-04 12:17:51 -0300

    remove update_data

--- stockpile/data.py
+++ stockpile/data.py
@@ -12,9 +12,3 @@ def read_data(file_path: str=None):
         data = file.read()
         return json.loads(data)
 
-
-def update_data(to_update: dict, file_path: str=None):
-    data = read_data(file_path)
-
-    data.update(to_update)
-    write_data(data, file_path)
