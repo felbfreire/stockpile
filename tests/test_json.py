d105b310d6e18e80a1f09aa0d917c4b4690e8f46
Felipe Freire <felbfreire@proton.me>
2023-08-04 12:20:17 -0300

    update tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,6 @@
 import os
 
-from stockpile.data import write_data, read_data, update_data
+from stockpile.data import write_data, read_data
 
 
 TEST_FILE = 'json_test.json'
@@ -18,12 +18,3 @@ class TestData:
 
         assert expected_data == {"foo": "bar", "goo": "car"}
 
-
-    def test_update_data_updates_json_file(self):
-        initial_data = {'foo': 'bar'}
-        new_data = {'foo': 'lol'}
-
-        write_data(initial_data, TEST_FILE)
-        update_data(new_data, TEST_FILE)
-
-        assert read_data(TEST_FILE) == {"foo": "lol"}
